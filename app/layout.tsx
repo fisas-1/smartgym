e5b1b7b82b645305dada99bf130be2b37450a2a9
fisas-1 <marcal.fisas@plamarcell.net>
2026-04-24 12:29:07 +0200

    ec

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="ca">
       <body className={`${inter.className} antialiased bg-black text-white`}>
-        {children}
-        <Navigation />
+        <Providers>
+          {children}
+          <Navigation />
+        </Providers>
       </body>
     </html>
   );
